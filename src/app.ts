9d98a2c9aff48d4ed270fea4ef402720780dc025
Balázs Kovács <balazs.kovacs@stylersonline.com>
2020-05-04 23:47:45 +0200

    Set up routing

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import * as Koa from 'koa';
+import * as KoaRouter from 'koa-router';
 
 const app = new Koa();
+const router = new KoaRouter();
 
-app.use(async ctx => ctx.body = 'Hello World');
+router.get('/', async ctx => ctx.body = { hello: 'world' });
+router.get('/test', async ctx => ctx.body = { hello: 'test' });
+
+app.use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3000, () => console.log('Server has started!'));
