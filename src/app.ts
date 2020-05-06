0d768580dc5c46de2336efc77c310ce957f7efc2
Balázs Kovács <balazs.kovacs@stylersonline.com>
2020-05-06 22:45:30 +0200

    Add templating

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import * as Koa from 'koa';
 import * as KoaRouter from 'koa-router';
+import * as render from 'koa-ejs';
+import * as path from 'path';
 
 const app = new Koa();
 const router = new KoaRouter();
 
-router.get('/', async ctx => ctx.body = { hello: 'world' });
-router.get('/test', async ctx => ctx.body = { hello: 'test' });
+render(app, {
+  root: path.join(__dirname, 'view'),
+  layout: 'layout',
+  viewExt: 'ejs',
+  cache: false,
+  debug: true
+});
+
+router.get('/', async ctx => {
+  await ctx.render('index');
+});
+router.get('/hello', async ctx => ctx.body = { hello: 'world' });
 
 app.use(router.routes())
   .use(router.allowedMethods());
