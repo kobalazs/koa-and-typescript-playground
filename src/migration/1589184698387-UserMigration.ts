cb8a59c2571c92411a43325692f2568a869a50aa
Balázs Kovács <balazs.kovacs@stylersonline.com>
2020-05-11 10:20:27 +0200

    Run `npm run typeorm -- migration:create -n UserMigration`, add `users` table fields, run `npm run typeorm migration:run`

---
+++ src/migration/1589184698387-UserMigration.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class UserMigration1589184698387 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(new Table({
+      name: 'users',
+      columns: [
+        {
+          name: 'id',
+          type: 'integer',
+          isPrimary: true
+        },
+        {
+          name: 'name',
+          type: 'varchar',
+        },
+        {
+          name: 'email',
+          type: 'varchar',
+        },
+        {
+          name: 'password',
+          type: 'varchar',
+        },
+      ]
+    }), true)
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable('users');
+  }
+
+}
